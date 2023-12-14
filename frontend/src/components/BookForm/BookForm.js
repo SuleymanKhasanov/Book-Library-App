a9cf9ce4baf7fab264e0d46e6a46b8f8de1051df
Сулейман Хасанов <macbook@MacBook-Air.local>
2023-12-14 11:47:55 +0500

    Создан компонент добавления книг с отображением в списке

--- frontend/src/components/BookForm/BookForm.js
+++ frontend/src/components/BookForm/BookForm.js
@@ -1,15 +1,25 @@
 import React from "react";
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addBook } from "./../../redux/books/actionCreators";
 import "./BookForm.css";
 
 function BookForm() {
   const [title, setTitle] = useState("");
   const [autor, setAutor] = useState("");
+  const dispatch = useDispatch();
 
   const hendlleSubmit = (e) => {
     e.preventDefault();
 
     if (title && autor) {
+      const book = {
+        title,
+        autor,
+      };
+
+      dispatch(addBook(book));
+
       setTitle("");
       setAutor("");
     }
@@ -20,28 +30,20 @@ function BookForm() {
       <h2>Add new book</h2>
       <form onSubmit={hendlleSubmit}>
         <div className="book-form">
-          <label htmlFor="title-input__text" className="title-input__lable">
+          <label htmlFor="title" className="title-input__lable">
             Book Name:
           </label>
-          <input
-            type="text"
-            id="title-input__text"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
+          <input type="text" id="title" value={title} onChange={(e) => setTitle(e.target.value)} />
         </div>
         <div className="title-autor">
-          <label htmlFor="title-autor__text" className="title-autor__lable">
+          <label htmlFor="autor" className="title-autor__lable">
             Autor Name:
           </label>
-          <input
-            type="text"
-            id="title-autor__text"
-            value={autor}
-            onChange={(e) => setAutor(e.target.value)}
-          />
+          <input type="text" id="autor" value={autor} onChange={(e) => setAutor(e.target.value)} />
         </div>
-        <button className="submit-btn">Submit</button>
+        <button className="submit-btn" type="submit">
+          Submit
+        </button>
       </form>
     </div>
   );
--- frontend/src/components/BookList/BookList.js
+++ frontend/src/components/BookList/BookList.js
@@ -1,10 +1,25 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import "./BookList.css";
 
 function BookList() {
+  const books = useSelector((state) => state.books);
   return (
     <div className="app-block book-list">
       <h2>Book list</h2>
+      {books.length === 0 ? (
+        <p>No avalible books</p>
+      ) : (
+        <ul>
+          {books.map((book, index) => (
+            <li key={index}>
+              <div className="book-info">
+                {++index}. {book.title} by <strong>{book.autor}</strong>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
